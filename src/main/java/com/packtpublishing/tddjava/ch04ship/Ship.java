ea732f6d9fe29e311fd992dcd3ec59dc27f7c3a7
Ronald Arias <ronald.arias@thoughtworks.com>
2020-07-08 22:05:47 -0500

    You are given the initial starting point (x, y) of a ship and the direction (n, s, e, w) it is facing.

--- src/main/java/com/packtpublishing/tddjava/ch04ship/Ship.java
+++ src/main/java/com/packtpublishing/tddjava/ch04ship/Ship.java
@@ -2,4 +2,13 @@ package com.packtpublishing.tddjava.ch04ship;
 
 public class Ship {
 
+    private final Location location;
+
+    public Ship(Location location) {
+        this.location = location;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
 }
--- src/test/java/com/packtpublishing/tddjava/ch04ship/ShipSpec.java
+++ src/test/java/com/packtpublishing/tddjava/ch04ship/ShipSpec.java
@@ -7,4 +7,17 @@ import static org.testng.Assert.*;
 @Test
 public class ShipSpec {
 
+    private Ship ship;
+    private Location location;
+
+    @BeforeMethod
+    public void beforeTest() {
+        location = new Location(new Point(11, 91), Direction.EAST);
+        ship = new Ship(location);
+    }
+
+    public void whenInstantiatedThenLocationIsSet() {
+        assertEquals(ship.getLocation(), location);
+    }
+
 }
